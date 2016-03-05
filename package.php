63165c68b6d238ad281b0afc91d72ba41047e520
Kayila <arekuanubis@gmail.com>
2016-03-05 03:12:18 -0500

    Initial commit

---
+++ package.php
@@ -0,0 +1,11 @@
+<?php
+
+$packages[] = array(
+    'name' => 'MLP1.7',
+    'title' => 'Mine Little Pony 1.7',
+    'version' => 'VERSION',
+    'priority' => 0,
+    'location' => 'modpacks/mlp1.7.json',
+);
+
+?>
